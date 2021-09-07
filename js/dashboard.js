27e20aef3b4cf9f9c0cabbf4607673f115b3f72a
Debraj Banik <56389271+debraj5399@users.noreply.github.com>
2021-09-07 23:36:04 +0530

    Add files via upload

---
+++ js/dashboard.js
@@ -0,0 +1,8 @@
+function myFunc() {
+    var x = document.getElementById("text");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
