edf3c0619dd80f6c735c17c3a5e53761013cb7af
riapaul98 <65454313+riapaul98@users.noreply.github.com>
2021-09-16 15:43:55 +0530

    Add files via upload

--- js/clock.js
+++ js/clock.js
@@ -25,7 +25,7 @@ function updateClock(){
         }
 
         var months = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"];
-        var week = ["Sun", "Mon", "Tue", "Wed", "Thurs", "Fri", "Sat"];
+        var week = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
         var ids = ["dayname", "month", "daynum", "year", "hour", "minutes", "seconds", "period"];
         var values = [week[dname], months[mo], dnum, yr, hou.pad(2), min.pad(2), sec.pad(2), pe];
         for(var i = 0; i < ids.length; i++)
