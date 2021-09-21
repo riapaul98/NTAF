3fb76bc148bf5094ea9bd01a2a34e20c4d43bab7
Ria Paul <ria17041998@gmail.com>
2021-09-22 02:05:30 +0530

    initial gui

--- js/chart.js
+++ js/chart.js
@@ -1,133 +1,76 @@
-// function myFunc2() {
-//   var xValues = ["CPU", "GPU", "RAM", "Memory", "Threads"];
-//   var yValues = [55, 49, 44, 24, 15];
-//   var barColors = ["#b91d47", "#00aba9", "#2b5797", "#e8c3b9", "#1e7145"];
+function myFunc1() {
+  var xValues = ["CPU", "GPU", "RAM", "Memory", "Threads"];
+  var yValues = [55, 49, 44, 24, 15];
+  var barColors = ["#b91d47", "#00aba9", "#2b5797", "#e8c3b9", "#1e7145"];
 
-//   new Chart("myChart", {
-//     type: "pie",
-//     data: {
-//       labels: xValues,
-//       datasets: [
-//         {
-//           backgroundColor: barColors,
-//           data: yValues,
-//         },
-//       ],
-//     },
-//     options: {
-//       title: {
-//         display: true,
-//         text: "PC Status",
-//       },
-//     },
-//   });
+  new Chart("myChart", {
+    type: "pie",
+    data: {
+      labels: xValues,
+      datasets: [
+        {
+          backgroundColor: barColors,
+          data: yValues,
+        },
+      ],
+    },
+    options: {
+      title: {
+        display: true,
+        text: "PC Status",
+      },
+    },
+  });
 
-//   var barColors = ["red", "green", "blue", "orange", "brown"];
+  var barColors = ["red", "green", "blue", "orange", "brown"];
 
-//   new Chart("myChart2", {
-//     type: "bar",
-//     data: {
-//       labels: xValues,
-//       datasets: [
-//         {
-//           backgroundColor: barColors,
-//           data: yValues,
-//         },
-//       ],
-//     },
-//     options: {
-//       legend: { display: false },
-//       title: {
-//         display: true,
-//         text: "GUI",
-//       },
-//     },
-//   });
-// }
-function myFunc2() {
-
-var chart = new CanvasJS.Chart("chartContainer", {
-	animationEnabled: true,
-	title:{
-		text: "Test Case Vs Time"
-	},
-	axisY :{
-		includeZero: false,
-	},
-	toolTip: {
-		shared: false
-	},
-	legend: {
-		fontSize: 14
-	},
-	data: [{
-		type: "splineArea", 
-		showInLegend: true,
-		name: "Total",
-		yValueFormatString: "$#,##0",
-		dataPoints: [
-			{ x: new Date(2021, 2), y: 5},
-			{ x: new Date(2021, 3), y: 3 },
-			{ x: new Date(2021, 4), y: 2 },
-			{ x: new Date(2021, 5), y: 7 },
-			{ x: new Date(2021, 6), y: 10 },
-			{ x: new Date(2021, 7), y: 14 },
-			{ x: new Date(2021, 8), y: 11 },
-			{ x: new Date(2021, 9), y: 25 },
-			{ x: new Date(2021, 10), y: 9 },
-			{ x: new Date(2021, 11), y: 4 },
-			{ x: new Date(2022, 0), y: 25 },
-			{ x: new Date(2022, 1), y: 25 }
-		]
- 	},
-	{
-		type: "splineArea", 
-		showInLegend: true,
-		name: "Pass",
-		yValueFormatString: "$#,##0",     
-		dataPoints: [
-			{ x: new Date(2021, 2), y: 4 },
-			{ x: new Date(2021, 3), y: 2 },
-			{ x: new Date(2021, 4), y: 2 },
-			{ x: new Date(2021, 5), y: 5 },
-			{ x: new Date(2021, 6), y: 7 },
-			{ x: new Date(2021, 7), y: 11 },
-			{ x: new Date(2021, 8), y: 8 },
-			{ x: new Date(2021, 9), y: 19 },
-			{ x: new Date(2021, 10), y: 7 },
-			{ x: new Date(2021, 11), y: 2 },
-			{ x: new Date(2022, 0), y: 24 },
-			{ x: new Date(2022, 1), y: 23 }
-		]
- 	},
-	{
-		type: "splineArea", 
-		showInLegend: true,
-		yValueFormatString: "$#,##0",      
-		name: "Fail",
-		dataPoints: [
-			{ x: new Date(2021, 2), y: 1 },
-			{ x: new Date(2021, 3), y: 1 },
-			{ x: new Date(2021, 4), y: 0 },
-			{ x: new Date(2021, 5), y: 2 },
-			{ x: new Date(2021, 6), y: 3 },
-			{ x: new Date(2021, 7), y: 3 },
-			{ x: new Date(2021, 8), y: 3 },
-			{ x: new Date(2021, 9), y: 6 },
-			{ x: new Date(2021, 10), y: 2 },
-			{ x: new Date(2021, 11), y: 2 },
-			{ x: new Date(2022, 0), y: 1 },
-			{ x: new Date(2022, 1), y: 2 }
-		]
-	}]
-});
-chart.render();
-
+  new Chart("myChart2", {
+    type: "bar",
+    data: {
+      labels: xValues,
+      datasets: [
+        {
+          backgroundColor: barColors,
+          data: yValues,
+        },
+      ],
+    },
+    options: {
+      legend: { display: false },
+      title: {
+        display: true,
+        text: "GUI",
+      },
+    },
+  });
 }
-// </script>
-// </head>
-// <body>
-// <div id="chartContainer" style="height: 300px; width: 100%;"></div>
-// <script type="text/javascript" src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
-// </body>
-// </html>
+function myFunc2() {
+  google.charts.load("current", { packages: ["corechart"] });
+  google.charts.setOnLoadCallback(drawChart);
+
+  function drawChart() {
+    var data = google.visualization.arrayToDataTable([
+      ["Time", "Total", "Pass", "Fail"],
+      ["09/09/21", 10, 7, 3],
+      ["10/09/21", 6, 3, 3],
+      ["11/09/21", 7, 6, 1],
+      ["14/09/21", 11, 8, 3],
+      ["15/09/21", 15, 9, 6],
+      ["19/09/21", 14, 12, 2],
+      ["20/09/21", 5, 3, 2],
+      ["21/09/21", 9, 8, 1],
+      ["25/09/21", 12, 2, 10],
+    ]);
+
+    var options = {
+      title: "Test Case Vs Time",
+      hAxis: { title: "Time", titleTextStyle: { color: "#333" } },
+      vAxis: { minValue: 0 },
+    };
+
+    var chart = new google.visualization.AreaChart(
+      document.getElementById("chart_div")
+    );
+    chart.draw(data, options);
+  }
+}
--- js/dashboard.js
+++ js/dashboard.js
@@ -1,10 +1,20 @@
 // alert button script
 
 function myFunc() {
-    var x = document.getElementById("text");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
+  var x = document.getElementById("text");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function loginEnter() {
+  var input = document.getElementById("inputPassword");
+  input.addEventListener("keypress", function (event) {
+    if (KeyboardEvent.code === 13) {
+        // console.log(KeyboardEvent.code);
+        event.preventDefault();
+        document.getElementById("enterLogin").click();
     }
+  });
 }
