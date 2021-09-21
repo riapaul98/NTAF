310a2161d5180be88ceaf0ba0a816bf96e7842c4
Debraj Banik <56389271+debraj5399@users.noreply.github.com>
2021-09-21 16:08:20 +0530

    Add files via upload

--- js/chart.js
+++ js/chart.js
@@ -53,13 +53,12 @@ var chart = new CanvasJS.Chart("chartContainer", {
 	},
 	axisY :{
 		includeZero: false,
-		prefix: "$"
 	},
 	toolTip: {
-		shared: true
+		shared: false
 	},
 	legend: {
-		fontSize: 13
+		fontSize: 14
 	},
 	data: [{
 		type: "splineArea", 
@@ -67,18 +66,18 @@ var chart = new CanvasJS.Chart("chartContainer", {
 		name: "Total",
 		yValueFormatString: "$#,##0",
 		dataPoints: [
-			{ x: new Date(2016, 2), y: 20100 },
-			{ x: new Date(2016, 3), y: 16000 },
-			{ x: new Date(2016, 4), y: 14000 },
-			{ x: new Date(2016, 5), y: 18000 },
-			{ x: new Date(2016, 6), y: 18000 },
-			{ x: new Date(2016, 7), y: 21000 },
-			{ x: new Date(2016, 8), y: 22000 },
-			{ x: new Date(2016, 9), y: 25000 },
-			{ x: new Date(2016, 10), y: 23000 },
-			{ x: new Date(2016, 11), y: 25000 },
-			{ x: new Date(2017, 0), y: 26000 },
-			{ x: new Date(2017, 1), y: 25000 }
+			{ x: new Date(2021, 2), y: 5},
+			{ x: new Date(2021, 3), y: 3 },
+			{ x: new Date(2021, 4), y: 2 },
+			{ x: new Date(2021, 5), y: 7 },
+			{ x: new Date(2021, 6), y: 10 },
+			{ x: new Date(2021, 7), y: 14 },
+			{ x: new Date(2021, 8), y: 11 },
+			{ x: new Date(2021, 9), y: 25 },
+			{ x: new Date(2021, 10), y: 9 },
+			{ x: new Date(2021, 11), y: 4 },
+			{ x: new Date(2022, 0), y: 25 },
+			{ x: new Date(2022, 1), y: 25 }
 		]
  	},
 	{
@@ -87,18 +86,18 @@ var chart = new CanvasJS.Chart("chartContainer", {
 		name: "Pass",
 		yValueFormatString: "$#,##0",     
 		dataPoints: [
-			{ x: new Date(2016, 2), y: 10100 },
-			{ x: new Date(2016, 3), y: 6000 },
-			{ x: new Date(2016, 4), y: 3400 },
-			{ x: new Date(2016, 5), y: 4000 },
-			{ x: new Date(2016, 6), y: 9000 },
-			{ x: new Date(2016, 7), y: 3900 },
-			{ x: new Date(2016, 8), y: 4200 },
-			{ x: new Date(2016, 9), y: 5000 },
-			{ x: new Date(2016, 10), y: 14300 },
-			{ x: new Date(2016, 11), y: 12300 },
-			{ x: new Date(2017, 0), y: 8300 },
-			{ x: new Date(2017, 1), y: 6300 }
+			{ x: new Date(2021, 2), y: 4 },
+			{ x: new Date(2021, 3), y: 2 },
+			{ x: new Date(2021, 4), y: 2 },
+			{ x: new Date(2021, 5), y: 5 },
+			{ x: new Date(2021, 6), y: 7 },
+			{ x: new Date(2021, 7), y: 11 },
+			{ x: new Date(2021, 8), y: 8 },
+			{ x: new Date(2021, 9), y: 19 },
+			{ x: new Date(2021, 10), y: 7 },
+			{ x: new Date(2021, 11), y: 2 },
+			{ x: new Date(2022, 0), y: 24 },
+			{ x: new Date(2022, 1), y: 23 }
 		]
  	},
 	{
@@ -107,18 +106,18 @@ var chart = new CanvasJS.Chart("chartContainer", {
 		yValueFormatString: "$#,##0",      
 		name: "Fail",
 		dataPoints: [
-			{ x: new Date(2016, 2), y: 1700 },
-			{ x: new Date(2016, 3), y: 2600 },
-			{ x: new Date(2016, 4), y: 1000 },
-			{ x: new Date(2016, 5), y: 1400 },
-			{ x: new Date(2016, 6), y: 900 },
-			{ x: new Date(2016, 7), y: 1000 },
-			{ x: new Date(2016, 8), y: 1200 },
-			{ x: new Date(2016, 9), y: 5000 },
-			{ x: new Date(2016, 10), y: 1300 },
-			{ x: new Date(2016, 11), y: 2300 },
-			{ x: new Date(2017, 0), y: 2800 },
-			{ x: new Date(2017, 1), y: 1300 }
+			{ x: new Date(2021, 2), y: 1 },
+			{ x: new Date(2021, 3), y: 1 },
+			{ x: new Date(2021, 4), y: 0 },
+			{ x: new Date(2021, 5), y: 2 },
+			{ x: new Date(2021, 6), y: 3 },
+			{ x: new Date(2021, 7), y: 3 },
+			{ x: new Date(2021, 8), y: 3 },
+			{ x: new Date(2021, 9), y: 6 },
+			{ x: new Date(2021, 10), y: 2 },
+			{ x: new Date(2021, 11), y: 2 },
+			{ x: new Date(2022, 0), y: 1 },
+			{ x: new Date(2022, 1), y: 2 }
 		]
 	}]
 });
